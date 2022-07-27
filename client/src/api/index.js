bbe3f25c418df5219bfcf1def7f9a429bd3470b5
aryan <aryanp862002@gmail.com>
2022-07-27 13:23:03 -0400

    first commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API = axios.create({ baseURL: 'http://localhost:5000'});
+const API = axios.create({ baseURL: process.env.REACT_APP_DEPLOY_ADDRESS});
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('profile')){
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
